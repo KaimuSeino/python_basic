49d06bbcd7938bdae246c184ef2b2ee82298df2f
KaimuSeino <skim07a19yki@icloud.com>
2024-04-28 18:39:02 +0900

    プログラミングの問題を解いた

--- 3,Control_flow/56独自例外の作成.py
+++ 3,Control_flow/56独自例外の作成.py
@@ -1,17 +1,23 @@
-# raise IndexError("インデックスエラーだぞ！気をつけたまえ！")
+# # raise IndexError("インデックスエラーだぞ！気をつけたまえ！")
 
-class UppercaseError(Exception):
-  pass
+# class UppercaseError(Exception):
+#   pass
 
-def check():
-  words = ["APPLE", "orange", "banana"]
-  for word in words:
-    if word.isupper():
-      raise UppercaseError(word)
+# def check():
+#   words = ["APPLE", "orange", "banana"]
+#   for word in words:
+#     if word.isupper():
+#       raise UppercaseError(word)
 
-try:
+# try:
 
-  check()
+#   check()
 
-except UppercaseError as ex:
-  print("This is my error")
+# except UppercaseError as ex:
+#   print("This is my error")
+
+n = 10
+if n <= 10:
+  print("low")
+else:
+  print(n)
